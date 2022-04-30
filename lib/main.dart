d7edea2356fd3ddd038d81ba12c54992d98198ba
정준영 <jeongjy@mrblue.com>
2022-04-30 21:18:48 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:baseball/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home : HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
